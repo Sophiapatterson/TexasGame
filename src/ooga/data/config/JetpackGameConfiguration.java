298569143055a3f96888224b678cb2144ab383a2
Sophia Patterson <Sophia.patterson@duke.edu>
2020-04-18 03:50:04 -0400

    debug in JJ

--- src/ooga/data/config/JetpackGameConfiguration.java
+++ src/ooga/data/config/JetpackGameConfiguration.java
@@ -40,7 +40,6 @@ public class JetpackGameConfiguration extends GameConfiguration {
     @Override
     public void makeCoin(double xCoef, double yCoef) {
         Coin pu = new Coin(xCoef*length, yCoef*1000);
-        pu.setYPos(JetpackGameWorld.FLOOR_HEIGHT);
         scrollers.add(pu);
         allPU.add(pu);
     }
