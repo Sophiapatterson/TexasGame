caac80d05553f1a6b107547cd1c75e0401c431bb
Luke Evans <lae32@duke.edu>
2020-04-24 00:08:49 -0400

    config redone

--- src/ooga/data/config/DinoGameConfiguration.java
+++ src/ooga/data/config/DinoGameConfiguration.java
@@ -9,6 +9,7 @@ import ooga.engine.game.Scrolling;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,18 +23,8 @@ public class DinoGameConfiguration extends GameConfiguration {
         scrollers = new ArrayList<>();
         allEnemies = new ArrayList<>();
         allPU = new ArrayList<>();
-        List<String> lines = null;
-
-        try {
-            lines = Files.readAllLines(path);
-        } catch (IOException e){
-            throw new LevelFileException(e);
-        }
-
-        String[] array;
-        length = Integer.parseInt(lines.get(0));
-        lines.remove(0);
-
+        List<String> lines = getLines(path);
+        length = getLength(lines);
         parseCSV(lines);
     }
 
--- src/ooga/data/config/GameConfiguration.java
+++ src/ooga/data/config/GameConfiguration.java
@@ -1,8 +1,14 @@
 package ooga.data.config;
 
+import ooga.data.LevelFileException;
 import ooga.engine.game.Enemy;
 import ooga.engine.game.Powerup;
 import ooga.engine.game.Scrolling;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 public abstract class GameConfiguration {
@@ -10,6 +16,8 @@ public abstract class GameConfiguration {
     public static final String ENEMY_VALUE = "1";
     public static final String COIN_VALUE = "3";
     public static final String DATA_FILE_REGEX = ",";
+    public static final int DEFAULT_LENGTH = 30000;
+    public static final String DEFAULT_LEVEL_CSV = "data/CSV configurations/Dinosaur_Level.csv";
 
     protected void parseCSV(List<String> lines) {
         String[] array;
@@ -26,7 +34,7 @@ public abstract class GameConfiguration {
             if(array.length == 0) break;
             totalCols = array.length;
             for(String element: array){
-                if(element.isEmpty()) break;
+                if(element.isEmpty()) continue;
                 val = element;
                 xCoef= (double)countCol/totalCols;
                 yCoef = (double)countRow/totalRow;
@@ -46,10 +54,44 @@ public abstract class GameConfiguration {
         }
     }
 
+    public int getLength(List<String> lines){
+        int length;
+        try {
+            length = Integer.parseInt(lines.get(0));
+            lines.remove(0);
+            return length;
+        }catch (NumberFormatException numEx){
+            System.out.println("Level length couldn't be parsed. Using default length.");
+            return DEFAULT_LENGTH;
+        }
+    }
+
+    public List<String> getLines(Path path){
+        List<String> lines = null;
+        try {
+            lines = Files.readAllLines(path);
+            try{
+                if(lines.size()==0){
+                    lines = Files.readAllLines(Paths.get(DEFAULT_LEVEL_CSV));
+                    System.out.println("Level csv couldn't be parsed. Using default csv.");
+                }
+            }catch (IOException e){
+                throw new LevelFileException("The level csv was blank and the default level csv couln't be accessed", e);
+            }
+        } catch (IOException e){
+            try {
+                lines = Files.readAllLines(Paths.get(DEFAULT_LEVEL_CSV));
+                System.out.println("Level csv couldn't be parsed. Using default csv.");
+            } catch (IOException ex) {
+                throw new LevelFileException("The level csv and default level csv both couldn't be accessed", ex);
+            }
+        }
+        return lines;
+    }
+
     public abstract void makeCoin(double xCoef, double yCoef);
     public abstract void makeEnemy(double xCoef, double yCoef);
 
-    //TODO is this allowed idk
     public abstract List<Scrolling> getScrollers();
 
     public abstract List<Enemy> getEnemies();
