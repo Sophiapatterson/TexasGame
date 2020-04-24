5a2d5998871005cc35d9ad6ba68c3fccc836549d
Justin Wu <jaw117@duke.edu>
2020-04-24 15:58:00 -0600

    Finished tests for tutorial

--- test/ooga/TutorialsTest.java
+++ test/ooga/TutorialsTest.java
@@ -7,6 +7,8 @@ import javafx.stage.Stage;
 import ooga.Screens.StartScreen;
 import ooga.Screens.TutorialScreen;
 import ooga.engine.dinosaur.DinoGameWorld;
+import ooga.engine.flappy.FlappyGameWorld;
+import ooga.engine.jetpack.JetpackGameWorld;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -20,17 +22,9 @@ public class TutorialsTest extends DukeApplicationTest{
     public static final int SCREEN_WIDTH = 850;
     public static final int SCREEN_HEIGHT = 600;
     private DinoGameWorld dinogame;
+    private FlappyGameWorld flappygame;
+    private JetpackGameWorld jetpackgame;
     private Stage myStage;
-
-    /**@Override
-    public void start(Stage primaryStage) throws IOException {
-        myStage = primaryStage;
-        myStage.setTitle("Texas");
-        StartScreen startMenu = new StartScreen();
-        Scene firstscene = startMenu.createMainScreen(myStage);
-        myStage.setScene(firstscene);
-        myStage.show();
-    }*/
 
     @Test
     void testDinoTutorial(){
@@ -40,8 +34,6 @@ public class TutorialsTest extends DukeApplicationTest{
                 showDinoTutorial();
                 Text message0 = lookup("#message0").query();
                 assertEquals("Press Space to jump over the cactus!",message0.getText());
-                System.out.println(myStage.getScene().getRoot().getChildrenUnmodifiable());
-                System.out.println(message0.getText());
             }
         });
     }
@@ -53,4 +45,41 @@ public class TutorialsTest extends DukeApplicationTest{
         myStage.show();
     }
 
+    @Test
+    void testFlappyTutorial(){
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                showFlappyTutorial();
+                Text message0 = lookup("#message0").query();
+                assertEquals("Press Space to keep flying and go through the pipes!",message0.getText());
+            }
+        });
+    }
+
+    private void showFlappyTutorial(){
+        myStage = new Stage();
+        flappygame = new FlappyGameWorld();
+        myStage.setScene(flappygame.setupScene(SCREEN_WIDTH, SCREEN_HEIGHT, BACKGROUND, myStage, true));
+        myStage.show();
+    }
+
+    @Test
+    void testJetpackTutorial(){
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                showJetpackTutorial();
+                Text message0 = lookup("#message0").query();
+                assertEquals("Press Space to keep flying and avoid the zappers!",message0.getText());
+            }
+        });
+    }
+    
+    private void showJetpackTutorial(){
+        myStage = new Stage();
+        jetpackgame = new JetpackGameWorld();
+        myStage.setScene(jetpackgame.setupScene(SCREEN_WIDTH, SCREEN_HEIGHT, BACKGROUND, myStage, true));
+        myStage.show();
+    }
 }
