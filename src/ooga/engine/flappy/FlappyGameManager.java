1061301813fdc0d4acde5018de1eaedd8a9f1b1f
Sophia Patterson <Sophia.patterson@duke.edu>
2020-04-25 01:52:52 -0400

    comments for coins, pipes, gamemanagers, players

--- src/ooga/engine/flappy/FlappyGameManager.java
+++ src/ooga/engine/flappy/FlappyGameManager.java
@@ -7,25 +7,42 @@ import ooga.engine.game.Powerup;
 
 import java.util.List;
 
+/**
+ * FlappyGameManager Class extends GameManager and is a specified
+ * version of GameManager for FlappyGame.
+ */
 public class FlappyGameManager extends GameManager {
     private Player bird;
     private List<Enemy> pipes;
     private List<Powerup> powerups;
     private boolean gameOver = false;
     private int score;
-
+    /**
+     * FlappyGameManager constructor consolidates information of Game's Player, Enemies, and Powerups.
+     * Initializes score to zero at start of game.
+     * @param bird BirdPlayer used in game.
+     * @param pipes List of Pipes in game.
+     * @param powerups List of Coins in game.
+     */
     public FlappyGameManager(Player bird, List<Enemy> pipes, List<Powerup> powerups) {
         this.bird = bird;
         this.pipes = pipes;
         this.powerups = powerups;
         score = INIT_SCORE;
     }
-
+    /**
+     * boolean isGameOver represents whether the the game is over.
+     * @return true if the game is over, false if the game is not over.
+     */
     @Override
     public boolean isGameOver() {
         return gameOver;
     }
-
+    /**
+     * handleCollisions method loops through all enemies and determines whether
+     * any of them is colliding with the player. When player fails to dodge an
+     * enemy, gameOver is set to true to indicate the end of the game.
+     */
     @Override
     public void handleCollisions() {
         for(Enemy pipe: pipes) {
@@ -34,7 +51,11 @@ public class FlappyGameManager extends GameManager {
             }
         }
     }
-
+    /**
+     * handlePowerups method handles the powerups in the game. There are no Powerups in the current version
+     * of FlappyGame, therefore the method returns null.
+     * @return null
+     */
     @Override
     public List<Powerup> handlePowerups() {
         return null;
@@ -45,17 +66,25 @@ public class FlappyGameManager extends GameManager {
 //            }
 //        }
     }
-
+    /**
+     * tick method increments the score as the game continues.
+     */
     @Override
     public void tick() {
         score += SCORE_TICK;
     }
-
+    /**
+     * getter method that returns the current score. Used in tick method.
+     * @return int score that represents the current score.
+     */
     @Override
     public int getScore() {
         return score;
     }
-
+    /**
+     * handleJump method handles a Player's jump.
+     * @param floorY double y-value representing the game's floor.
+     */
     @Override
     // if the bird falls onto the floor, we also end the game
     public void handleJump(double floorY) {
--- src/ooga/engine/flappy/Pipe2.java
+++ src/ooga/engine/flappy/Pipe2.java
@@ -19,65 +19,110 @@ public class Pipe2 extends Enemy {
     private int xOffset = 100;
     private int playerOffset = 40;
     private final int standardY = -61;
-
+    /**
+     * Pipe2 constructor creates new Pipe2 object.
+     * @param x double that initializes the Pipe2's x double property.
+     * @param y double that initializes the Pipe2's y double property.
+     */
     public Pipe2(double x, double y) {
         super();
         this.x.setValue(x);
         this.y.setValue(y);
     }
-
+    /**
+     * Additional Pipe2 constructor.
+     */
     public Pipe2(){
         super();
     }
-
+    /**
+     * scroll method used to make Pipe2 move across screen.
+     */
     @Override
     public void scroll() {
         setXPos(getXPos() - SPEED);
     }
-
+    /**
+     * sets Pipe2 object's X-coordinate
+     * @param x desired X-coordinate
+     */
     @Override
     public void setXPos(double x) {
         this.x.setValue(x);
     }
-
+    /**
+     * sets Pipe2 object's Y-coordinate
+     * @param y desired Y-coordinate
+     */
     @Override
     public void setYPos(double y){
         this.y.setValue(y);
     }
-
+    /**
+     * gets Pipe2 object's X-coordinate
+     * @return double x that represents the Pipe2's X-coordinate.
+     */
     @Override
     public double getXPos(){ return x.getValue(); }
-
+    /**
+     * gets Pipe2 object's Y-coordinate
+     * @return double y that represents the Pipe2's Y-coordinate.
+     */
     @Override
     public double getYPos(){ return y.getValue(); }
-
+    /**
+     * getXProperty returns the Pipe's X double property.
+     * @return x which is the Pipe's X double property.
+     */
     @Override
     public DoubleProperty getXProperty(){
         return x;
     }
-
+    /**
+     * collide method implemented to fit Pipe object parameters.
+     * method determines whether the player is colliding with a Pipe.
+     * @param player the BirdPlayer in the game.
+     * @return isColliding a boolean that represents whether the Player and Pipe are colliding.
+     */
     @Override
     public boolean collide(Player player) {
         return inXBounds(player) && inYBounds(player);
     }
-
+    /**
+     * boolean inXBounds helper method for collide method. Determines whether BirdPlayer and Pipe have overlapping x-values.
+     * @param player BirdPlayer in current game.
+     * @return boolean whether Pipe and player are colliding.
+     */
     private boolean inXBounds(Player player) {
         return player.getXPos() > this.getXPos() - playerOffset && player.getXPos() < this.getXPos() + xOffset - playerOffset;
     }
-
+    /**
+     * boolean inYBounds helper method for collide method. Determines whether BirdPlayer and Pipe have overlapping y-values.
+     * @param player BirdPlayer in current game.
+     * @return boolean whether Pipe and player are colliding.
+     */
     private boolean inYBounds(Player player) {
         return (player.getYPos() < upperHalfY || player.getYPos() > lowerHalfY);
     }
-
+    /**
+     * getImage method returns the Pipe's Sprite path.
+     * @return String that indicates where Pipe image is stored.
+     */
     public String getImage() {
         return image;
     }
-
+    /**
+     * setStandardY method used to set an Pipe2's position to line up
+     * with the Y-coordinate of FlappyGame's floor.
+     */
     @Override
     public void setStandardY(){
         setYPos(standardY);
     }
-
+    /**
+     * getYProperty returns the Pipe's Y double property.
+     * @return y which is the Pipe's Y double property.
+     */
     @Override
     public DoubleProperty getYProperty(){
         return y;
